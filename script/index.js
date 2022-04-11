57ec68d049a34b36eb90df0621f4ee5ba3dbcf5b
motylist <moty.listovsky@gmail.com>
2022-04-11 14:55:42 +0300

    finish develop, ready to merge to main

--- script/index.js
+++ script/index.js
@@ -2,14 +2,30 @@ const modalDisplay = document.querySelector(".modal");
 const modalOpen = document.querySelector(".profile__button_type_edit");
 const modalClose = document.querySelector(".modal__button_type_close");
 const likeButton = document.querySelector(".photo-grid__card-label-icon");
+let nameInput = document.querySelector(".form__input_type_name");
+let jobInput = document.querySelector(".form__input_type_job");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
+const form = document.querySelector(".modal__form");
 
-function openModal() {
-  modalDisplay.classList.remove("modal__opened");
+function toggleModal() {
+  modalDisplay.classList.toggle("modal__opened");
 }
 
-function closeModal() {
-  modalDisplay.classList.add("modal__opened");
-}
+modalOpen.addEventListener("click", function () {
+  toggleModal();
 
-modalOpen.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+
+modalClose.addEventListener("click", toggleModal);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
+  toggleModal();
+});
